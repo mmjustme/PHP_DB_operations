72a94adfbe1275d1118f1f9a083e617e54d4ed3f
mmjustme <infooneuser@gmail.com>
2024-07-12 19:14:59 +0300

    add 20,21 lesson

---
+++ 20_connect_db/includes/dbh.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=myfirst_db";
+$dbusername = "root";
+$dbpassword = "";
+
+// pdo - php data object - good method to connect
+// also here is "sql-i" 
+try {
+    // create db object 
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+
+    // error handle
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION);
+} catch (PDOExeption $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
---
+++ 21_connect_db_insert_values/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Work with db, insert data</h1>     
+
+    <form action="includes/formhandler.inc.php" method="post">
+        <h3>Signup</h3>
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="E-Mail">
+        <button>Signup</button>
+    </form>
+
+</body>
+</html>
