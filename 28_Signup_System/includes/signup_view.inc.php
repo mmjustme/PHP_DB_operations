859b32b81aa7d5a30f3b4b5f9652817813e83a4f
mmjustme <infooneuser@gmail.com>
2024-07-19 21:45:00 +0300

    Add signup success message

--- 28_Signup_System/includes/signup_view.inc.php
+++ 28_Signup_System/includes/signup_view.inc.php
@@ -16,8 +16,13 @@ function check_signup_errors() {
         foreach ($errors as $error){
             echo '<p class="form-error">' . $error .'</p>';
         }
-
         # після копії помилок ми очищаємо дані з сесії
         unset($_SESSION["errors_signup"]);
+
+    } else if(isset($_GET["signup"]) && $_GET["signup"] === "success") {
+        # при успішнову ств юзера ми передапвали в дрес строці signup=success
+        # тут ми й перевіряємо це і виводимо повідомлення нижче
+        echo '<br>';
+        echo '<p>Signup success!</p>';
     }
 }
