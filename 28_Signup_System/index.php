a529c402f09bf1ab73c1a502cf5f980093d61afe
mmjustme <infooneuser@gmail.com>
2024-07-20 18:07:31 +0300

    update index php and hide or show log in system

--- 28_Signup_System/index.php
+++ 28_Signup_System/index.php
@@ -1,40 +1,56 @@
 <?php
-    require_once "includes/config_session.inc.php";
-    require_once "includes/signup_view.inc.php";
-    require_once "includes/login_view.inc.php";
+require_once "includes/config_session.inc.php";
+require_once "includes/signup_view.inc.php";
+require_once "includes/login_view.inc.php";
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' href="css/style.css">
     <title>Signup</title>
 </head>
+
 <body>
-    
-    <h3>Login</h3>
-    <form action="includes/login.inc.php" method="post">       
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password">        
-        <button>Login</button>
-    </form>
+    <h3><?php output_username() ?></h3>
+
+    <?php if (!isset($_SESSION["user_id"])) { ?>
+
+        <h3>Login</h3>
+        <form action="includes/login.inc.php" method="post">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="pwd" placeholder="Password">
+            <button>Login</button>
+        </form>
+
+    <?php } ?>
+
+
+
 
     <?php
-        check_login_errors();
+    check_login_errors();
     ?>
 
     <h3>Signup</h3>
-    <form action="includes/signup.inc.php" method="post">      
-          <?php signup_inputs() ?>
+    <form action="includes/signup.inc.php" method="post">
+        <?php signup_inputs() ?>
         <button>Signup</button>
     </form>
 
     <?php
-        check_signup_errors();
+    check_signup_errors();
     ?>
 
+    <h3>Logout</h3>
+    <form action="includes/logout.inc.php" method="post">
+        <button>Logout</button>
+    </form>
+
 </body>
+
 </html>
