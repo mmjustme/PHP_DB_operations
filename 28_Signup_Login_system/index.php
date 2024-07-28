4d042bb69c71640bb18f679026cefea1c9b41ee2
mmjustme <infooneuser@gmail.com>
2024-07-28 10:30:12 +0300

    1. Create basic forms for sign and log in. 2. Create DB connection. 3. Create session config. 4. Add some css style.

---
+++ 28_Signup_Login_system/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href="css/style.css">
+    <title>Signup</title>
+</head>
+
+<body>
+
+    <h3>Login</h3>
+    <form action="" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <button>Login</button>
+    </form>
+
+    <h3>Signup</h3>
+    <form action="" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="Email">
+        <button>Signup</button>
+    </form>
+
+</body>
+
+</html>
