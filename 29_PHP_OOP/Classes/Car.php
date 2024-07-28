99d900e5dd719bc7e6b3d20d02e64d10fbcf7b3e
mmjustme <infooneuser@gmail.com>
2024-07-28 21:26:15 +0300

    Classes: 1. Procedural vs OOP 2. Basics about Class

--- 29_PHP_OOP/Classes/Car.php
+++ 29_PHP_OOP/Classes/Car.php
@@ -2,35 +2,47 @@
 
 class Car
 {
-    // Properties / Fields
-    # protected - підкласи будуть мати доступ до змінної
-    # private - доступ до змінної є лише в даного класу
-    # public - доступ до змінної є в усіх
+    # Properties / Fields
+
+    # protected - даний клас та підкласи будуть мати доступ до змінних
+    # private - доступ до змінних є лише в даного класу
+    # public - доступ до змінної є в усіх    
+    # важливо робити змінні private, protected і лише в конкретних випадках public    
     private $brand;
     private $color;
     public $vehicleType = "car";
 
-    // Constructor
+    # Constructor
     public function __construct($brand, $color = "none")
     {
         $this->brand = $brand;
         $this->color = $color;
     }
 
-    // Method
-
+    # Methods
     public function getCarInfo()
     {
+        # створення власних методів через $this що звертається до класу
         return 'Brand: ' . $this->brand . ', Color: ' . $this->color;
     }
 }
 
+# приклади створення класів, виведемо на index.php тому тут закоментовано
+$car01 = new Car("Volvo", "white");
+# зверення до змінної класу
+# оскільки vehicleType - public ми маємо можливість звернутися
+echo $car01->vehicleType; // car
+echo "<br>";
+# інші параметри private і звернення викличе помилку
+// echo $car01->brand; // Uncaught Error: Cannot access private property Car
 
-// $car01 = new Car("Volvo", "white");
-// $car02 = new Car("BMW");
+$car02 = new Car("BMW","black");
+echo $car02->getCarInfo(); // Brand: BMW, Color: black
+echo "<br>";
 
-// // echo $car01->vehicleType;
-// echo "<br>";
-// echo $car01->getCarInfo();
-// echo "<br>";
-// echo $car01->vehicleType;
+# стоверно без color, створиться за замовчуванням "none" як ми вказали в конструкторі
+# в іншому випадку буде помилка
+$car03 = new Car("Toyota");
+# зверення до методу класу
+echo "car03 - ", $car03->getCarInfo(); // Brand: Toyota, Color: none
+
--- 29_PHP_OOP/index.php
+++ 29_PHP_OOP/index.php
@@ -10,24 +10,35 @@
 <body>
     <h1>OOP</h1>
 
+    <h3>Procedural Example</h3>
+
     <?php
+    # function example or procedural 
+    $brand = "Volvo";
+    $color = "Green";
 
-    // $brand = "Volvo";
-    // $color = "Green";
+    function gertCarInfo($brand, $color)
+    {
+        echo 'Brand: ' . $brand . ', Color: ' . $color;
+    }
 
-    // function gertCarInfo($brand, $color)
-    // {
-    //     echo 'Brand: ' . $brand . ', Color: ' . $color;
-    // }
+    echo gertCarInfo($brand, $color);
+    ?>
 
-    // echo gertCarInfo($brand, $color);
+    <h3>OOP Example</h3>
 
+    <?php
+    echo "---Car.php","<br>";
     require_once "Classes/Car.php";
-    $car05 = new Car("Toyota","black");
-    echo"<br>";
-    echo $car05->vehicleType;
-    echo"<br>";
-    echo $car05->getCarInfo();
+
+    echo "<br>","<br>","<br>";
+    $car05 = new Car("Toyota", "black");
+
+    echo "---index.php","<br>";
+    # оскіоль
+    echo "car05 - vehicleType - ",$car05->vehicleType; // car05 - vehicleType - car
+    echo "<br>";
+    echo "car05 - ", $car05->getCarInfo(); // car05 - Brand: Toyota, Color: black
     ?>
 </body>
 
