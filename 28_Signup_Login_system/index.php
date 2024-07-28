1b6460f00934d7a5a6f07c44057169a869c93ba9
mmjustme <infooneuser@gmail.com>
2024-07-28 13:15:01 +0300

    Update css

--- 28_Signup_Login_system/index.php
+++ 28_Signup_Login_system/index.php
@@ -11,7 +11,7 @@ require_once "includes/views/signup_view.inc.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href="css/style.css">
+    <link rel='stylesheet' href="css/style2.css">
     <title>Signup</title>
 </head>
 
