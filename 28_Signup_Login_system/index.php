cb4142a182a2e6c9555b0eceecae96a0906f518a
D <166647954+mmjustme@users.noreply.github.com>
2024-08-08 13:02:48 +0000

    Login: 1. Show error after login.

--- 28_Signup_Login_system/index.php
+++ 28_Signup_Login_system/index.php
@@ -3,6 +3,7 @@
 require_once "includes/config_session.inc.php";
 # підключаємо для показу помилок
 require_once "includes/views/signup_view.inc.php";
+require_once "includes/views/login_view.inc.php";
 ?>
 
 <!DOCTYPE html>
@@ -23,6 +24,10 @@ require_once "includes/views/signup_view.inc.php";
         <input type="password" name="pwd" placeholder="Password">
         <button>Login</button>
     </form>
+
+    <?php
+    check_login_errors();
+    ?>
 
     <h3>Signup</h3>
     <form action="includes/signup.inc.php" method="post">
