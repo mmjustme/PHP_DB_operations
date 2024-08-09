7ba579eff9c4d1e15769883c108e6d9b88affa24
D <166647954+mmjustme@users.noreply.github.com>
2024-08-09 12:33:40 +0000

    OOP: 1. Create classes Dbh for connect to db, and start Signup.

---
+++ 30_p2_OOP/Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h3>Signup</h3>
+    <form action="includes/signup.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <button>Signup</button>
+    </form>
+</body>
+</html>
