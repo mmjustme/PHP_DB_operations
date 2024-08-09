e12235eeade24a11ed93fefafff4b306ba358397
mmjustme <infooneuser@gmail.com>
2024-08-09 20:33:56 +0300

    BUGS

--- 28_Signup_Login_system/includes/dbh.inc.php
+++ 28_Signup_Login_system/includes/dbh.inc.php
@@ -9,6 +9,6 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOExeption $e) {
+} catch (PDOException $e) {
     die("Connection failed: ". $e->getMessage());
 }
