05e2e0a664eb8f80f28bf2bb95d8efce7268c136
mmjustme <infooneuser@gmail.com>
2024-08-11 10:28:19 +0300

    Classes: 1. Signup class method insert user

--- 30_p2_OOP/Classes/Signup.php
+++ 30_p2_OOP/Classes/Signup.php
@@ -1,7 +1,8 @@
 <?php
 
-# тепер Signup підклас класу Dbh
-# а отже, Signup має можливість звернутися до методів Dbh класу
+# тепер Signup child класу Dbh, тобто Dbh батьківсткий клас для Signup
+# а отже, Signup має можливість звернутися до зметодів(навіть protected) Dbh класу
+# також важливий порядок файлів в signup.inc.php
 class Signup extends Dbh
 {
     private $username;
@@ -13,4 +14,19 @@ class Signup extends Dbh
         $this->pwd = $pwd;
     }
 
+
+    private function connect(){}
+
+    private function insertUser() {
+        $query = "INSERT INTO users ('username', 'pwd') VALUES (':username', ':pwd');";
+
+        # якщо є класи з однаковими назвами, замість $this можна викр. parent
+        # оскільки connect повертає pdo то ми може звернутися до prepare        
+        $stmt=parent::connect()->prepare($query);
+        // $stmt=$this->connect()->prepare($query); // викличе помилку оск вже існує метод connect вище
+
+        $stmt->bindParam(":username", $this->username);
+        $stmt->bindParam(":pwd", $this->pwd);
+        $stmt->execute();
+    }
 }
